54dc26b9f8821df01e10fc1b70ad10153a1bac18
Dishant Sthapit <dishant.sthapit@bigtincan.com>
2019-08-08 18:05:55 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.63fc37823fb9bb8fc285213324a7d2d9.js"
+  "/portfolio/precache-manifest.4d77eaf2f9fcb9fc652be57ffccb4e23.js"
 );
 
 self.addEventListener('message', (event) => {
