9e4ab99a5287ecb8a42a29549674a549d24d498c
Dishant Sthapit <dishant.sthapit@bigtincan.com>
2019-09-25 16:06:37 +1000

    Modal update
    Modal update

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,18 +1,68 @@
 import React from 'react';
 import "./index.css";
 import experienceData from './../../json/experienceData';
-import {Row,Col} from 'react-bootstrap';
+import {Row,Col,Modal} from 'react-bootstrap';
+
 
 
 class Experience extends React.Component {
 	constructor() {
 		super();
+		this.close = this.close.bind(this);
+		this.open = this.open.bind(this);
+		this.state = {
+			showModal: false
+		};
+	}
+
+	getInitialState() {
+		return { showModal: false };
+	}
+	
+	close() {
+	this.setState({ showModal: false });
+	}
+
+	open() {
+	this.setState({ showModal: true });
 	}
 
 	render() {
 		return (
 			<div className="container">
 				<div className="section-heading">Experience</div>
+					<Modal 
+						show={this.state.showModal} 
+						onHide={this.close} 
+						size="lg"
+						aria-labelledby="contained-modal-title-vcenter"
+						centered
+						dialogClassName="modal-90w"
+					>
+						<Modal.Header>
+							<Modal.Title>Detail Information</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>
+							{experienceData.map((item) => {
+								return (
+									<div>
+										<ul>{item.summary.title}
+											<li>{item.description.skills}</li>
+											<li>{item.summary.summary1}</li>
+											<li>{item.summary.summary2}</li>
+											<li>{item.summary.summary3}</li>
+										</ul>
+									</div>
+			)
+							})
+							}
+							
+						</Modal.Body>
+						<Modal.Footer>
+							<button className="modal-button modal-select-button" onClick={this.close}>Cancel</button>
+
+						</Modal.Footer>
+					</Modal>
 
 				<Row>
 				{experienceData.map((item) => {
@@ -26,7 +76,8 @@ class Experience extends React.Component {
 									<h2>{item.description.title}</h2>
 									<div className="experience-cw-title">{item.role}</div>
 									<div className="experience-cw-duration">{item.description.time}</div>
-									<button>Find out more</button>
+									
+									<button onClick={this.open}>Find out more</button>
 								</div>
 							</div>
 						</Col>
